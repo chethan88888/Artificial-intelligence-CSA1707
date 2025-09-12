d8fe1aba677c5a88f60146b479542e484ab37fc4
chethan88888 <gubalachethan@gmail.com>
2025-09-12 13:44:22 +0530

    Create exp3.py

---
+++ exp3.py
@@ -0,0 +1,17 @@
+x, y = 0, 0  # (4L, 3L)
+print("Steps (4L,3L):")
+print(x, y)
+
+y = 3
+print(x, y)
+
+x, y = 3, 0
+print(x, y)
+
+y = 3
+print(x, y)
+
+x, y = 4, 2  # final
+print(x, y)
+
+print("Final State: 4L jug has 2 liters")
